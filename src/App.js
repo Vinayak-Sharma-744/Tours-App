f315e3dc0e24b92e4646903f432a4c1a5c4d626d
Akash <akashdogra171@gmail.com>
2023-07-20 21:21:44 +0530

    xyz

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Components/Loading'
-import Tours from './Components/Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Components/Loading";
+import Tours from "./Components/Tours";
 
-
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
-	const [tours, setTours] = useState([])
-	const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-	async function fetchTours() {
-		// set loading to true here
-		setLoading(true)
+  async function fetchTours() {
+    // set loading to true here
+    setLoading(true);
 
-		const data = await fetch(url)
-		const jsonData = await data.json()
-		setTours(jsonData)
+    const data = await fetch(url);
+    const jsonData = await data.json();
+    setTours(jsonData);
 
-		setLoading(false)
-		// set loading to false here
-	}
+    setLoading(false);
+    // set loading to false here
+  }
 
-	useEffect(() => {
-		fetchTours()
-	}, [])
+  useEffect(() => {
+    fetchTours();
+  }, []);
 
-	if (loading) {
-		return (
-			<main>
-				<Loading />
-			</main>
-		)
-	}
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
 
-	return (
-		<main>
-			<Tours tours={tours} />
-		</main>
-	)
+  return (
+    <main>
+      <Tours tours={tours} />
+    </main>
+  );
 }
 
-export default App
+export default App;
