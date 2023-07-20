20ce6d6908f00a28cedc3ed72b96621a65e8b637
Vinayak-Sharma-744 <sharmavinayak744@gmail.com>
2023-07-20 21:22:24 +0530

    tour changes

--- src/Components/Tour.jsx
+++ src/Components/Tour.jsx
@@ -1,21 +1,19 @@
 const Tour = (props) => {
-	const { id, image, info, price, name } = props
-	return (
-		<>
-			<article className="single-tour">
-				<img src= {props.image} alt={props.name} />
-				<footer>
-					<div className="tour-info">
-						<h4>{props.name}</h4>
-						<h4 className="tour-price">{props.price}</h4>
-					</div>
-					<p>{props.info}</p>
-					<button className="delete-btn">Not Interested</button>
-				</footer>
-			</article>
+  const { id, image, info, price, name } = props;
+  return (
+    <>
+      <article className="single-tour">
+        <img src={image} alt={name} />
+        <footer>
+          <div className="tour-info">
+            <h4>{name}</h4>
+            <h4 className="tour-price">{price}</h4>
+          </div>
+          <p>{info}</p>
+        </footer>
+      </article>
+    </>
+  );
+};
 
-		</>
-	);
-}
-
-export default Tour
+export default Tour;
