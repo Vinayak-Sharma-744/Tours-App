5d184b42890b54211a9f2272c8ebef2af8ea5fdd
Vinayak-Sharma-744 <sharmavinayak744@gmail.com>
2023-07-20 21:26:46 +0530

    ..

--- src/Components/Tour.jsx
+++ src/Components/Tour.jsx
@@ -3,13 +3,13 @@ const Tour = (props) => {
 	return (
 		<>
 			<article className="single-tour">
-				<img src= {props.image} alt={props.name} />
+				<img src= {image} alt={name} />
 				<footer>
 					<div className="tour-info">
-						<h4>{props.name}</h4>
-						<h4 className="tour-price">{props.price}</h4>
+						<h4>{name}</h4>
+						<h4 className="tour-price">{price}</h4>
 					</div>
-					<p>{props.info}</p>
+					<p>{info}</p>
 					<button className="delete-btn">Not Interested</button>
 				</footer>
 			</article>
