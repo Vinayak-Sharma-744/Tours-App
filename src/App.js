07f880285e1945900f77c4ec44ff4474fca08476
Vinayak-Sharma-744 <sharmavinayak744@gmail.com>
2023-07-20 21:08:08 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-import Tour from './Tour'
+import Loading from './Components/Loading'
+import Tours from './Components/Tours'
+
 
 const url = 'https://course-api.com/react-tours-project'
 
