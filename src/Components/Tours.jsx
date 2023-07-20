a06265e9ce5ce247ac1b6119e06211ec81d00279
Vinayak-Sharma-744 <sharmavinayak744@gmail.com>
2023-07-20 21:09:44 +0530

    .

--- src/Components/Tours.jsx
+++ src/Components/Tours.jsx
@@ -7,7 +7,6 @@ const Tours = (props) => {
 		<section>
 			<div className="title">
 				<h2>All Tours</h2>
-				<div className="underline"></div>
 			</div>
 			<div>
 				{props.tours.map((tour, index) => {
